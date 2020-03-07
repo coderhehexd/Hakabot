53e53e296e668e8d9169dccf4c67c4bcb2791b7b
coderhehexd <42257328+coderhehexd@users.noreply.github.com>
2020-03-07 17:34:29 -0500

    Init bot

---
+++ myBot.py
@@ -0,0 +1,37 @@
+#aioBot.py
+
+''' =====================IMPORTS================='''
+
+import os
+import discord
+from dotenv import load_dotenv
+
+'''======================IMPORTS================='''
+
+#Create client connect to discord server
+load_dotenv()
+#TOKEN = os.getenv('DISCORD_TOKEN')
+#GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = 'VZH36BcDLPNadeitYjyQB5I87hSA2mbv'
+GUILD = 'test_bot'
+
+client = discord.Client()
+'''
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+    for guilds in Client.guilds:
+        if(guild.name == GUILD):
+            break
+    print(f'{client.user} is connected to the following guild:{guild.name}(id: {guild.id})\n')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'{Guild Members: \n - {members}')
+'''
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+client.run(TOKEN)
